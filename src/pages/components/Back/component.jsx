c09e3b5bec52e730e7390729c29cd94c81468d3c
Masha <masha.sladky@gmail.com>
2023-01-16 22:01:05 +0200

    added back css styles

--- src/pages/components/Back/component.jsx
+++ src/pages/components/Back/component.jsx
@@ -4,8 +4,8 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const Back = () => {
     return (
-        <NavLink to={'/'}>
-            <IconButton>
+        <NavLink to={'/'} className='back'>
+            <IconButton className='back_btn'>
                 <ArrowBackIcon/>
                 Back
             </IconButton>
